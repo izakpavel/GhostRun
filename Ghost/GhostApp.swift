4365863df85d973d36823fa101ca2235eecd0724
pzak <pzak@fio.cz>
2020-10-28 08:38:29 +0100

    ghost shape and mouth animations

---
+++ Ghost/GhostApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostApp.swift
+//  Ghost
+//
+//  Created by Pavel Zak on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GhostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
